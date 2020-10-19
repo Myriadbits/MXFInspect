4634f39c733b93d2bf53bd47531ff9a5c8bba50f
ft <16635037+rayden84@users.noreply.github.com>
2020-10-19 17:12:37 +0200

    add type electrospatialformulation

--- MXF/Metadata/F05 MXFGenericSoundEssenceDescriptor.cs
+++ MXF/Metadata/F05 MXFGenericSoundEssenceDescriptor.cs
@@ -26,65 +26,65 @@ using System.ComponentModel;
 
 namespace Myriadbits.MXF
 {
-	public class MXFGenericSoundEssenceDescriptor : MXFFileDescriptor
-	{
-		[CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D03")]
-		public MXFRational AudioSamplingRate { get; set; }
-		[CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D02")]
-		public bool? Locked { get; set; }
-		[CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D04")]
-		public sbyte? AudioRefLevel { get; set; }
-		[CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D05")]
-		public byte? ElectroSpatialFormulation { get; set; }
-		[CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D07")]
-		public UInt32? ChannelCount { get; set; }
-		[CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D01")]
-		public UInt32? QuantizationBits { get; set; }
-		[CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D0C")]
-		public sbyte? DialNorm { get; set; }
-		[CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D06")]
-		public MXFKey SoundEssenceCoding { get; set; }
-		
-		
-		/// <summary>
-		/// Constructor, set the correct descriptor name
-		/// </summary>
-		/// <param name="reader"></param>
-		/// <param name="headerKLV"></param>
-		public MXFGenericSoundEssenceDescriptor(MXFReader reader, MXFKLV headerKLV)
-			: base(reader, headerKLV, "Generic Sound Essence Descriptor")
-		{
-		}
+    public class MXFGenericSoundEssenceDescriptor : MXFFileDescriptor
+    {
+        [CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D03")]
+        public MXFRational AudioSamplingRate { get; set; }
+        [CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D02")]
+        public bool? Locked { get; set; }
+        [CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D04")]
+        public sbyte? AudioRefLevel { get; set; }
+        [CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D05")]
+        public MXFElectroSpatialFormulation ElectroSpatialFormulation { get; set; }
+        [CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D07")]
+        public UInt32? ChannelCount { get; set; }
+        [CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D01")]
+        public UInt32? QuantizationBits { get; set; }
+        [CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D0C")]
+        public sbyte? DialNorm { get; set; }
+        [CategoryAttribute("GenericSoundEssenceDescriptor"), Description("3D06")]
+        public MXFKey SoundEssenceCoding { get; set; }
 
-		/// <summary>
-		/// Constructor, set the correct descriptor name
-		/// </summary>
-		/// <param name="reader"></param>
-		/// <param name="headerKLV"></param>
-		public MXFGenericSoundEssenceDescriptor(MXFReader reader, MXFKLV headerKLV, string metadataName)
-			: base(reader, headerKLV, metadataName)
-		{
-		}
 
-		/// <summary>
-		/// Overridden method to process local tags
-		/// </summary>
-		/// <param name="localTag"></param>
-		protected override bool ParseLocalTag(MXFReader reader, MXFLocalTag localTag)
-		{
-			switch (localTag.Tag)
-			{
-				case 0x3D03: this.AudioSamplingRate = reader.ReadRational(); return true;
-				case 0x3D02: this.Locked = reader.ReadBool(); return true;
-				case 0x3D04: this.AudioRefLevel = reader.ReadSignedByte(); return true;
-				case 0x3D05: this.ElectroSpatialFormulation = reader.ReadByte(); return true;
-				case 0x3D07: this.ChannelCount = reader.ReadUInt32(); return true;
-				case 0x3D01: this.QuantizationBits = reader.ReadUInt32(); return true;
-				case 0x3D0C: this.DialNorm = reader.ReadSignedByte(); return true;
-				case 0x3D06: this.SoundEssenceCoding = reader.ReadKey(); return true;
-			}
-			return base.ParseLocalTag(reader, localTag);
-		}
+        /// <summary>
+        /// Constructor, set the correct descriptor name
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <param name="headerKLV"></param>
+        public MXFGenericSoundEssenceDescriptor(MXFReader reader, MXFKLV headerKLV)
+            : base(reader, headerKLV, "Generic Sound Essence Descriptor")
+        {
+        }
 
-	}
+        /// <summary>
+        /// Constructor, set the correct descriptor name
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <param name="headerKLV"></param>
+        public MXFGenericSoundEssenceDescriptor(MXFReader reader, MXFKLV headerKLV, string metadataName)
+            : base(reader, headerKLV, metadataName)
+        {
+        }
+
+        /// <summary>
+        /// Overridden method to process local tags
+        /// </summary>
+        /// <param name="localTag"></param>
+        protected override bool ParseLocalTag(MXFReader reader, MXFLocalTag localTag)
+        {
+            switch (localTag.Tag)
+            {
+                case 0x3D03: this.AudioSamplingRate = reader.ReadRational(); return true;
+                case 0x3D02: this.Locked = reader.ReadBool(); return true;
+                case 0x3D04: this.AudioRefLevel = reader.ReadSignedByte(); return true;
+                case 0x3D05: this.ElectroSpatialFormulation = (MXFElectroSpatialFormulation)reader.ReadByte(); return true;
+                case 0x3D07: this.ChannelCount = reader.ReadUInt32(); return true;
+                case 0x3D01: this.QuantizationBits = reader.ReadUInt32(); return true;
+                case 0x3D0C: this.DialNorm = reader.ReadSignedByte(); return true;
+                case 0x3D06: this.SoundEssenceCoding = reader.ReadKey(); return true;
+            }
+            return base.ParseLocalTag(reader, localTag);
+        }
+
+    }
 }
