556f7120a46f2e396de087d984f3ec5642a49ad6
ft <16635037+rayden84@users.noreply.github.com>
2020-10-31 21:33:03 +0100

    decode indexentry flags into its own class

--- MXF/Entries/MXFEntryIndex.cs
+++ MXF/Entries/MXFEntryIndex.cs
@@ -26,58 +26,98 @@ using System.ComponentModel;
 
 namespace Myriadbits.MXF
 {
-	public class MXFEntryIndex : MXFObject
-	{
-		[CategoryAttribute("IndexEntry"), ReadOnly(true)]
-		public UInt64 Index { get; set; }
-		[CategoryAttribute("IndexEntry"), ReadOnly(true)] 
-		public sbyte? TemporalOffset { get; set; }
-		[CategoryAttribute("IndexEntry"), ReadOnly(true)] 
-		public sbyte? KeyFrameOffset { get; set; }
-		[CategoryAttribute("IndexEntry"), ReadOnly(true)] 
-		public byte? Flags { get; set; }
-		[CategoryAttribute("IndexEntry"), ReadOnly(true)] 
-		public UInt64? StreamOffset { get; set; }
-		[CategoryAttribute("IndexEntry"), ReadOnly(true)]
-		public UInt32[] SliceOffsets { get; set; }
-		[CategoryAttribute("IndexEntry"), ReadOnly(true)]
-		public MXFRational[] PosTable { get; set; }
 
-		public MXFEntryIndex(UInt64 index, MXFReader reader, byte? sliceCount, byte? posTableCount, UInt32 length)
-			: base(reader)
-		{
-			this.m_eType = MXFObjectType.Index;
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public class IndexEntryFlags
+    {
+        private readonly byte _bitfield;
+        
+        public bool RandomAccess { get; private set; }
+        public bool SequenceHeader { get; private set; }
+        public bool ForwardPredictionFlag { get; private set; }
+        public bool BackwardPredictionFlag { get; private set; }
+        public bool NumericalRangeOverload { get; private set; }
+        public bool Reserved { get; private set; }
+        public MPEGPictureType MPEGPictureType { get; private set; }
 
-			this.Length = length;
-			this.Index = index;
-			this.TemporalOffset = reader.ReadSignedByte();
-			this.KeyFrameOffset = reader.ReadSignedByte();
-			this.Flags = reader.ReadByte();
-			this.StreamOffset = reader.ReadUInt64();
+        public IndexEntryFlags(byte bitfield)
+        {
+            _bitfield = bitfield;
+            RandomAccess = (0x80 & bitfield) != 0;
+            SequenceHeader = (0x40 & bitfield) != 0;
+            ForwardPredictionFlag = (0x20 & bitfield) != 0;
+            BackwardPredictionFlag = (0x10 & bitfield) != 0;
+            NumericalRangeOverload = (0x08 & bitfield) != 0;
+            Reserved = (0x04 & bitfield) != 0;
+            MPEGPictureType = (MPEGPictureType)(0x03 & bitfield);
+        }
 
-			if (sliceCount.HasValue && sliceCount.Value > 0)
-			{
-				this.SliceOffsets = new UInt32[sliceCount.Value];
-				for (int n = 0; n < sliceCount; n++)
-					this.SliceOffsets[n] = reader.ReadUInt32();
-			}
+        public override string ToString()
+        {
+            return Convert.ToString(_bitfield, 2).PadLeft(8, '0');
+        }
+    }
 
-			if (posTableCount.HasValue && posTableCount.Value > 0)
-			{
-				this.PosTable = new MXFRational[posTableCount.Value];
-				for (int n = 0; n < posTableCount; n++)
-					this.PosTable[n] = reader.ReadRational();				
-			}
-		}
+    public enum MPEGPictureType
+    {
+        I_Frame = 0x00,
+        P_Frame = 0x02,
+        B_Frame = 0x03
+    }
 
-		/// <summary>
-		/// Some output
-		/// </summary>
-		/// <returns></returns>
-		public override string ToString()
-		{
-			return string.Format("Index[{0}] - TemporalOffset {1}, KeyFrameOffset {2}, Offset {3}", this.Index, this.TemporalOffset, this.KeyFrameOffset, this.StreamOffset);
-		}
 
-	}
+    public class MXFEntryIndex : MXFObject
+    {
+        [CategoryAttribute("IndexEntry"), ReadOnly(true)]
+        public UInt64 Index { get; set; }
+        [CategoryAttribute("IndexEntry"), ReadOnly(true)]
+        public sbyte? TemporalOffset { get; set; }
+        [CategoryAttribute("IndexEntry"), ReadOnly(true)]
+        public sbyte? KeyFrameOffset { get; set; }
+        [CategoryAttribute("IndexEntry"), ReadOnly(true)]
+        public IndexEntryFlags Flags { get; set; }
+        [CategoryAttribute("IndexEntry"), ReadOnly(true)]
+        public UInt64? StreamOffset { get; set; }
+        [CategoryAttribute("IndexEntry"), ReadOnly(true)]
+        public UInt32[] SliceOffsets { get; set; }
+        [CategoryAttribute("IndexEntry"), ReadOnly(true)]
+        public MXFRational[] PosTable { get; set; }
+
+        public MXFEntryIndex(UInt64 index, MXFReader reader, byte? sliceCount, byte? posTableCount, UInt32 length)
+            : base(reader)
+        {
+            this.m_eType = MXFObjectType.Index;
+
+            this.Length = length;
+            this.Index = index;
+            this.TemporalOffset = reader.ReadSignedByte();
+            this.KeyFrameOffset = reader.ReadSignedByte();
+            this.Flags = new IndexEntryFlags(reader.ReadByte());
+            this.StreamOffset = reader.ReadUInt64();
+
+            if (sliceCount.HasValue && sliceCount.Value > 0)
+            {
+                this.SliceOffsets = new UInt32[sliceCount.Value];
+                for (int n = 0; n < sliceCount; n++)
+                    this.SliceOffsets[n] = reader.ReadUInt32();
+            }
+
+            if (posTableCount.HasValue && posTableCount.Value > 0)
+            {
+                this.PosTable = new MXFRational[posTableCount.Value];
+                for (int n = 0; n < posTableCount; n++)
+                    this.PosTable[n] = reader.ReadRational();
+            }
+        }
+
+        /// <summary>
+        /// Some output
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("Index[{0}] - TemporalOffset {1}, KeyFrameOffset {2}, Offset {3}", this.Index, this.TemporalOffset, this.KeyFrameOffset, this.StreamOffset);
+        }
+
+    }
 }
