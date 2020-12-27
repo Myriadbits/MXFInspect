9bd8bba766e991f1edf82a8c16bb295af8a83995
ft <rayden84@users.noreply.github.com>
2020-12-28 00:51:42 +0100

    fix propertygrid not being readonly

--- MXFInspect/ReadOnlyPropertyGrid.cs
+++ MXFInspect/ReadOnlyPropertyGrid.cs
@@ -21,7 +21,9 @@
 //
 #endregion
 
+using Myriadbits.MXF;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
@@ -58,26 +60,79 @@ namespace Myriadbits.MXFInspect
 		{
 			this.SetObjectAsReadOnly(this.SelectedObject, this._readOnly);
 			base.OnSelectedObjectsChanged(e);
+
 		}
 
 		private void SetObjectAsReadOnly(object selectedObject, bool isReadOnly)
 		{
 			if (selectedObject != null)
 			{
+				//TypeDescriptor.AddAttributes(selectedObject.GetType(), new ReadOnlyAttribute(true));
+
+				//TypeDescriptor.AddAttributes(selectedObject, new ShouldSerializeAttribute)
+
+				//if(selectedObject is MXFKLV klv)
+				//            {
+				//	TypeDescriptor.AddAttributes(klv.Key, new ReadOnlyAttribute(true));
+				//}
+				//TypeDescriptor.AddAttributes(selectedObject, new ReadOnlyAttribute(true));
 				//TypeDescriptor.GetProperties(this)["StringProperty"].SetReadOnlyAttribute(!editable);
 
-				foreach (PropertyDescriptor prop in TypeDescriptor.GetProperties(selectedObject))
-				{
-					prop.SetReadOnlyAttribute(isReadOnly);
-					//ReadOnlyAttribute attr = prop.Attributes[typeof(ReadOnlyAttribute)] as ReadOnlyAttribute;
-					//if (attr != null)
-					//{
-					//	FieldInfo fi = attr.GetType().GetField("isReadOnly", BindingFlags.NonPublic | BindingFlags.Instance);
-					//	if (fi != null)
-					//		fi.SetValue(attr, isReadOnly);
-					//}
-				}
-				this.Refresh();
+				TypeDescriptor.AddAttributes(selectedObject, new ReadOnlyAttribute(true));
+				var list = TypeDescriptor.GetProperties(selectedObject);
+
+				propExpandAndReadOnly(list);
+
+				//foreach (PropertyDescriptor prop in TypeDescriptor.GetProperties(selectedObject))
+    //            {
+    //                prop.SetReadOnlyAttribute(isReadOnly);
+    //                //var childProp = prop.PropertyType.GetProperties();
+    //                //               foreach (PropertyDescriptor cprop in childProp)
+    //                //               {
+    //                //	cprop.SetReadOnlyAttribute(isReadOnly);
+    //                //}
+    //                //ReadOnlyAttribute attr = prop.Attributes[typeof(ReadOnlyAttribute)] as ReadOnlyAttribute;
+    //                //if (attr != null)
+    //                //{
+    //                //    FieldInfo fi = attr.GetType().GetField("isReadOnly", BindingFlags.NonPublic | BindingFlags.Instance);
+    //                //    if (fi != null)
+    //                //        fi.SetValue(attr, isReadOnly);
+    //                //}
+    //            }
+                this.Refresh();
+			}
+		}
+
+		private void propExpandAndReadOnly(PropertyDescriptorCollection propertyDescriptorList)
+		{
+			var propList = propertyDescriptorList.Cast<PropertyDescriptor>().ToList();
+			var childpropList = propList.SelectMany(o => o.GetChildProperties().Cast<PropertyDescriptor>().ToList());
+
+			var entireList = propList.Concat(childpropList).Distinct();
+
+			//propList.First().ser
+
+			foreach (PropertyDescriptor pd in entireList)
+			{
+				pd.SetReadOnlyAttribute(true);
+                //propertyDescriptor.SetExpandableAttribute(true);
+
+    //            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(propertyDescriptor.PropertyType);
+				//var a = propertyDescriptor.GetChildProperties();
+    //            if (propertyDescriptor.IsBrowsable  && properties != null && properties.Count > 0)
+    //            {
+    //                propExpandAndReadOnly(properties);
+    //            }
+
+				//PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(propertyDescriptor.PropertyType);
+				//var a = pd.GetChildProperties();
+				//if (pd.IsBrowsable && a.Count>0)
+				//{
+				//	propExpandAndReadOnly(a);
+				//}
+				//DynamicTypeDescriptor typeDescriptor = new DefaultTypeDesc ((propertyDescriptor.PropertyType.Get);
+				//var chilPropertyDescriptorList = properties.Properties.Cast<PropertyDescriptor>().ToList();
+
 			}
 		}
 	}
@@ -86,12 +141,10 @@ namespace Myriadbits.MXFInspect
 	{
 		public static void SetReadOnlyAttribute(this PropertyDescriptor p, bool value)
 		{
-			var attributes = p.Attributes.Cast<Attribute>()
-				.Where(x => !(x is ReadOnlyAttribute)).ToList();
+			var attributes = p.Attributes.Cast<Attribute>().Where(x => !(x is ReadOnlyAttribute)).ToList();
 			attributes.Add(new ReadOnlyAttribute(value));
-			typeof(MemberDescriptor).GetProperty("AttributeArray",
-				BindingFlags.Instance | BindingFlags.NonPublic)
-				.SetValue((MemberDescriptor)p, attributes.ToArray());
+			var pi = typeof(MemberDescriptor).GetProperty("AttributeArray", BindingFlags.Instance | BindingFlags.NonPublic);
+			pi.SetValue(p, attributes.ToArray());
 		}
 	}
 
