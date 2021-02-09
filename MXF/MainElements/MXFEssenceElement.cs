5e450250846780a3084618546c5f4ea546271ea1
ft <rayden84@users.noreply.github.com>
2021-02-09 12:37:36 +0100

    remove static constructor in favor of static initializer

--- MXF/MainElements/MXFEssenceElement.cs
+++ MXF/MainElements/MXFEssenceElement.cs
@@ -25,70 +25,64 @@ using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace Myriadbits.MXF
-{	
-	public class MXFEssenceElement : MXFKLV
-	{
-		private const string CATEGORYNAME = "EssenceElement";
+{
+    public class MXFEssenceElement : MXFKLV
+    {
+        private const string CATEGORYNAME = "EssenceElement";
 
-		private static Dictionary<int, string> m_itemTypes = new Dictionary<int, string>();
+        private static Dictionary<int, string> m_itemTypes = new Dictionary<int, string>()
+        {
+            {0x05, "CP Picture (SMPTE 326M)"},
+            {0x06, "CP Sound (SMPTE 326M)"},
+            {0x07, "CP Data (SMPTE 326M)"},
+            {0x15, "GC Picture"},
+            {0x16, "GC Sound"},
+            {0x17, "GC Data"},
+            {0x18, "GC Compound"}
+        };
 
-		[Category(CATEGORYNAME)]
-		public string ItemType { get; set; }
-		[Category(CATEGORYNAME)]
-		public byte ElementCount { get; set; }
-		[Category(CATEGORYNAME)]
-		public byte ElementType { get; set; }
-		[Category(CATEGORYNAME)]
-		public byte ElementNumber { get; set; }
-		[Browsable(false)]
-		public bool IsPicture { get; set; }
+        [Category(CATEGORYNAME)]
+        public string ItemType { get; set; }
+        [Category(CATEGORYNAME)]
+        public byte ElementCount { get; set; }
+        [Category(CATEGORYNAME)]
+        public byte ElementType { get; set; }
+        [Category(CATEGORYNAME)]
+        public byte ElementNumber { get; set; }
+        [Browsable(false)]
+        public bool IsPicture { get; set; }
 
-		[Category(CATEGORYNAME)]
-		public long EssenceOffset
-		{
-			get
-			{
-				if (this.Partition == null) return this.Offset; // Unknown
-				if (this.Partition.FirstPictureEssenceElement == null) return this.Offset; // Unknown
-				return (this.Offset - this.Partition.FirstPictureEssenceElement.Offset) + ((long)this.Partition.BodyOffset);
-			}
-		}
+        [Category(CATEGORYNAME)]
+        public long EssenceOffset
+        {
+            get
+            {
+                if (this.Partition == null) return this.Offset; // Unknown
+                if (this.Partition.FirstPictureEssenceElement == null) return this.Offset; // Unknown
+                return (this.Offset - this.Partition.FirstPictureEssenceElement.Offset) + ((long)this.Partition.BodyOffset);
+            }
+        }
 
-		[Browsable(false)]
-		public bool Indexed { get; set; }
+        [Browsable(false)]
+        public bool Indexed { get; set; }
 
-		/// <summary>
-		/// Static constructor to initialize the static array
-		/// </summary>
-		static MXFEssenceElement()
-		{
-			// Add all meta data 
-			m_itemTypes.Add(0x05, "CP Picture (SMPTE 326M)");
-			m_itemTypes.Add(0x06, "CP Sound (SMPTE 326M)");
-			m_itemTypes.Add(0x07, "CP Data (SMPTE 326M)");
-			m_itemTypes.Add(0x15, "GC Picture");
-			m_itemTypes.Add(0x16, "GC Sound");
-			m_itemTypes.Add(0x17, "GC Data");
-			m_itemTypes.Add(0x18, "GC Compound");
-		}
+        public MXFEssenceElement(MXFReader reader, MXFKLV headerKLV)
+            : base(headerKLV, "EssenceElement", KeyType.Essence)
+        {
+            this.m_eType = MXFObjectType.Essence;
+            if (m_itemTypes.ContainsKey(this.Key[12]))
+                this.ItemType = m_itemTypes[this.Key[12]];
+            else
+                this.ItemType = "<unknown>";
+            this.IsPicture = (this.Key[12] == 0x05 || this.Key[12] == 0x15);
+            this.ElementCount = this.Key[13];
+            this.ElementType = this.Key[14];
+            this.ElementNumber = this.Key[15];
+        }
 
-		public MXFEssenceElement(MXFReader reader, MXFKLV headerKLV)
-			: base(headerKLV, "EssenceElement", KeyType.Essence)
-		{
-			this.m_eType = MXFObjectType.Essence;
-			if (m_itemTypes.ContainsKey(this.Key[12]))
-				this.ItemType = m_itemTypes[this.Key[12]];
-			else
-				this.ItemType = "<unknown>";
-			this.IsPicture = (this.Key[12] == 0x05 || this.Key[12] == 0x15);
-			this.ElementCount = this.Key[13];
-			this.ElementType = this.Key[14];
-			this.ElementNumber = this.Key[15];
-		}
-		
-		public override string ToString()
-		{
-			return string.Format("{0} Essence [len {1}]", this.ItemType, this.Length);
-		}
-	}
+        public override string ToString()
+        {
+            return string.Format("{0} Essence [len {1}]", this.ItemType, this.Length);
+        }
+    }
 }
