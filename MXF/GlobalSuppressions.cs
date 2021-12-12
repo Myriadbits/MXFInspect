746261af361f4f5114f57193fc148e23ad2b79f0
feigenanton <feigeanton@hotmail.de>
2021-12-12 17:44:44 +0100

    add suppression for Message IDE0090 'new' expression can be simplified

--- MXF/GlobalSuppressions.cs
+++ MXF/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "<Pending>", Scope = "type", Target = "~T:Myriadbits.MXF.MXFPartition")]
+[assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "<Pending>", Scope = "module")]
