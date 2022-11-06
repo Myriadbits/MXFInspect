adf2aa2111a5dacd195da76657e0e67d9baaa825
feigenanton <feigeanton@hotmail.de>
2022-11-06 16:51:46 +0100

    miminal fixes in MXFView

--- MXFInspect/MXFView.Designer.cs
+++ MXFInspect/MXFView.Designer.cs
@@ -71,8 +71,8 @@
             // splitMain.Panel2
             // 
             this.splitMain.Panel2.Controls.Add(this.splitRight);
-            this.splitMain.Size = new System.Drawing.Size(1108, 575);
-            this.splitMain.SplitterDistance = 525;
+            this.splitMain.Size = new System.Drawing.Size(1181, 575);
+            this.splitMain.SplitterDistance = 558;
             this.splitMain.SplitterWidth = 6;
             this.splitMain.TabIndex = 0;
             // 
@@ -86,7 +86,7 @@
             this.tabMain.Location = new System.Drawing.Point(4, 4);
             this.tabMain.Name = "tabMain";
             this.tabMain.SelectedIndex = 0;
-            this.tabMain.Size = new System.Drawing.Size(523, 572);
+            this.tabMain.Size = new System.Drawing.Size(556, 572);
             this.tabMain.TabIndex = 0;
             this.tabMain.SelectedIndexChanged += new System.EventHandler(this.tabMain_SelectedIndexChanged);
             // 
@@ -96,7 +96,7 @@
             this.tpPhysical.Location = new System.Drawing.Point(4, 24);
             this.tpPhysical.Name = "tpPhysical";
             this.tpPhysical.Padding = new System.Windows.Forms.Padding(3);
-            this.tpPhysical.Size = new System.Drawing.Size(515, 544);
+            this.tpPhysical.Size = new System.Drawing.Size(548, 544);
             this.tpPhysical.TabIndex = 0;
             this.tpPhysical.Text = "Physical";
             this.tpPhysical.UseVisualStyleBackColor = true;
@@ -104,19 +104,17 @@
             // tlvPhysical
             // 
             this.tlvPhysical.AlternateRowBackColor = System.Drawing.Color.WhiteSmoke;
-            this.tlvPhysical.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlvPhysical.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlvPhysical.EmptyListMsg = "No items present";
             this.tlvPhysical.EmptyListMsgFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.tlvPhysical.FullRowSelect = true;
-            this.tlvPhysical.Location = new System.Drawing.Point(6, 6);
+            this.tlvPhysical.Location = new System.Drawing.Point(3, 3);
             this.tlvPhysical.MultiSelect = false;
             this.tlvPhysical.Name = "tlvPhysical";
             this.tlvPhysical.RowHeight = 19;
             this.tlvPhysical.SelectedBackColor = System.Drawing.SystemColors.Highlight;
             this.tlvPhysical.ShowGroups = false;
-            this.tlvPhysical.Size = new System.Drawing.Size(503, 532);
+            this.tlvPhysical.Size = new System.Drawing.Size(542, 538);
             this.tlvPhysical.TabIndex = 15;
             this.tlvPhysical.TintSortColumn = true;
             this.tlvPhysical.UnfocusedSelectedBackColor = System.Drawing.SystemColors.Highlight;
@@ -134,7 +132,7 @@
             this.tpLogical.Location = new System.Drawing.Point(4, 24);
             this.tpLogical.Name = "tpLogical";
             this.tpLogical.Padding = new System.Windows.Forms.Padding(3);
-            this.tpLogical.Size = new System.Drawing.Size(515, 544);
+            this.tpLogical.Size = new System.Drawing.Size(548, 544);
             this.tpLogical.TabIndex = 1;
             this.tpLogical.Text = "Logical";
             this.tpLogical.UseVisualStyleBackColor = true;
@@ -154,7 +152,7 @@
             this.tlvLogical.RowHeight = 19;
             this.tlvLogical.SelectedBackColor = System.Drawing.SystemColors.Highlight;
             this.tlvLogical.ShowGroups = false;
-            this.tlvLogical.Size = new System.Drawing.Size(503, 532);
+            this.tlvLogical.Size = new System.Drawing.Size(536, 532);
             this.tlvLogical.TabIndex = 16;
             this.tlvLogical.TintSortColumn = true;
             this.tlvLogical.UnfocusedSelectedBackColor = System.Drawing.SystemColors.Highlight;
@@ -170,7 +168,7 @@
             this.splitRight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.splitRight.Location = new System.Drawing.Point(0, 0);
+            this.splitRight.Location = new System.Drawing.Point(3, 3);
             this.splitRight.Name = "splitRight";
             this.splitRight.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -181,7 +179,7 @@
             // splitRight.Panel2
             // 
             this.splitRight.Panel2.Controls.Add(this.rtfHexViewer);
-            this.splitRight.Size = new System.Drawing.Size(568, 576);
+            this.splitRight.Size = new System.Drawing.Size(608, 576);
             this.splitRight.SplitterDistance = 407;
             this.splitRight.SplitterWidth = 6;
             this.splitRight.TabIndex = 16;
@@ -193,11 +191,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.propGrid.DisabledItemForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(1)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.propGrid.HelpVisible = false;
-            this.propGrid.Location = new System.Drawing.Point(3, 26);
+            this.propGrid.Location = new System.Drawing.Point(3, 25);
             this.propGrid.Name = "propGrid";
             this.propGrid.PropertySort = System.Windows.Forms.PropertySort.Categorized;
             this.propGrid.ReadOnly = true;
-            this.propGrid.Size = new System.Drawing.Size(559, 378);
+            this.propGrid.Size = new System.Drawing.Size(599, 379);
             this.propGrid.TabIndex = 1;
             this.propGrid.ToolbarVisible = false;
             this.propGrid.ViewForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
@@ -215,7 +213,7 @@
             this.rtfHexViewer.Location = new System.Drawing.Point(3, 3);
             this.rtfHexViewer.Name = "rtfHexViewer";
             this.rtfHexViewer.ReadOnly = true;
-            this.rtfHexViewer.Size = new System.Drawing.Size(559, 156);
+            this.rtfHexViewer.Size = new System.Drawing.Size(599, 148);
             this.rtfHexViewer.TabIndex = 16;
             this.rtfHexViewer.Text = "";
             // 
@@ -226,14 +224,14 @@
             this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainPanel.Location = new System.Drawing.Point(0, 0);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(1111, 581);
+            this.mainPanel.Size = new System.Drawing.Size(1184, 581);
             this.mainPanel.TabIndex = 12;
             // 
             // MXFView
             // 
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1111, 581);
+            this.ClientSize = new System.Drawing.Size(1184, 581);
             this.Controls.Add(this.mainPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -266,7 +264,6 @@
 		#endregion
 
 		private System.Windows.Forms.SplitContainer splitMain;
-		private MXFInspect.ReadOnlyPropertyGrid propGrid;
 		private System.Windows.Forms.TabControl tabMain;
 		private System.Windows.Forms.TabPage tpPhysical;
 		private System.Windows.Forms.Panel mainPanel;
@@ -275,6 +272,7 @@
 		private HexViewer rtfHexViewer;
         private PhysicalTreeListView tlvPhysical;
         private LogicalTreeListView tlvLogical;
+        private ReadOnlyPropertyGrid propGrid;
     }
 }
 
