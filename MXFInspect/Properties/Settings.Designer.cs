9777474e142d59a639402e08818f4358554d1ffe
feigenanton <feigeanton@hotmail.de>
2023-01-09 14:02:13 +0100

    remove unused setting

--- MXFInspect/Properties/Settings.Designer.cs
+++ MXFInspect/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Myriadbits.MXFInspect.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,18 +20,6 @@ namespace Myriadbits.MXFInspect.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Filename {
-            get {
-                return ((string)(this["Filename"]));
-            }
-            set {
-                this["Filename"] = value;
             }
         }
         
