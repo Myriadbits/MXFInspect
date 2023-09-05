a2a1a5ffd1dd2ca0f14cecf0ad203258dca0b025
feigenanton <feigeanton@hotmail.de>
2023-09-05 23:24:22 +0200

    log the dotnet runtime too

--- MXFInspect/Program.cs
+++ MXFInspect/Program.cs
@@ -69,7 +69,8 @@ namespace Myriadbits.MXFInspect
             
             Log.Logger = loggerConfig.CreateLogger();
             Log.ForContext(typeof(Program)).Information($"Application started from '{Application.ExecutablePath}'");
-            Log.ForContext(typeof(Program)).Information($"Application Version: {Application.ProductVersion}");
+            Log.ForContext(typeof(Program)).Information($"Application version: {Application.ProductVersion}");
+            Log.ForContext(typeof(Program)).Information($"Runtime Version: {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}");
             Log.ForContext(typeof(Program)).Information($"Operating System: {Environment.OSVersion}");
             Log.ForContext(typeof(Program)).Information($"Current Username: {Environment.UserName}, Computer Name: {Environment.MachineName}");
             Log.ForContext(typeof(Program)).Information($"Log path: '{LogDirectoryPath}'");
