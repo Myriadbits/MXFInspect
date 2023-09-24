23062113e81a80409077ae6d1460a546b8f7c67e
feigenanton <feigeanton@hotmail.de>
2023-09-24 18:49:53 +0200

    change default color of special object

--- MXFInspect/Properties/Settings.Designer.cs
+++ MXFInspect/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Myriadbits.MXFInspect.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -156,7 +156,7 @@ namespace Myriadbits.MXFInspect.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("DarkRed")]
+        [global::System.Configuration.DefaultSettingValueAttribute("LightSeaGreen")]
         public global::System.Drawing.Color Color_Special {
             get {
                 return ((global::System.Drawing.Color)(this["Color_Special"]));
